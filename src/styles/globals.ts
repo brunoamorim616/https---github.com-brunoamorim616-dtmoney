75e3b55306c62c0104d2c19fae53b114755d5380
Bruno Amorim <amorimbq@gmail.com>
2022-04-13 21:40:06 -0300

    ...

---
+++ src/styles/globals.ts
@@ -0,0 +1,58 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+:root {
+  --background: #f0f2f5;
+  --shape: #FFFFFF;
+
+  --red: #E52E4D;
+  --blue: #5429cc;
+
+  --blue-light: #6933FF;
+
+  --text-title: #363F5F;
+  --text-body: #969CB3;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  @media (max-width: 1080px) {
+    font-size: 93.75%;
+  }
+  @media (max-width: 720px) {
+    font-size: 87.5%;
+  }
+}
+
+body {
+  background-color: var(--background);
+  -webkit-font-smoothing: antialiased;
+}
+
+body, button, input, textarea {
+  font-family: 'Popping', sans-serif;
+  font-weight: 400;
+}
+
+h2, h3, h4, h5, h6 {
+  font-weight: 600;
+}
+
+h1, strong{
+  font-weight: 700;
+}
+
+button {
+  cursor: pointer;
+}
+
+[disabled] {
+  opacity: 0.6;
+  cursor: not-allowed;
+}
+`;
